e1258e82126ce8bfd8a5f970845654ea928e9a80
esoft41 <rodrigosorlandini@gmail.com>
2023-11-02 15:40:12 -0300

    FEAT: Added theme configs

---
+++ src/components/ThemeRegistry/ThemeRegistry.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import * as React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { NextAppDirEmotionCacheProvider } from "./EmotionCache";
+import theme from "./theme";
+
+// Example from https://github.com/mui/material-ui/tree/master/examples/material-ui-nextjs-ts
+
+export function ThemeRegistry({ children }: { children: React.ReactNode }) {
+  return (
+    <NextAppDirEmotionCacheProvider options={{ key: "mui" }}>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </NextAppDirEmotionCacheProvider>
+  );
+}
